c165f775ae4f57afb3d0cf18595b135eae6b7dba
itayben37 <itay3711@gmail.com>
2024-08-25 12:00:50 +0300

    ש

--- basic.py
+++ basic.py
@@ -454,7 +454,7 @@ class Parser:
         return list, index + count
 
 
-    def delete_tokens(self, start_idx, end_idx):
+    def ב(self, start_idx, end_idx):
         del self.tokens[start_idx:end_idx]
         return end_idx - start_idx
 
